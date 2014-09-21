f9c8e4133a2c4cd9ee6c921a421d094147979a28
Carlton Stedman <sageman@gmail.com>
2014-09-21 17:24:46 -0400

    plot4 done

--- plot4.R
+++ plot4.R
@@ -13,21 +13,35 @@ scs <- readRDS("Source_Classification_Code.rds")
 
 # get SCC codes for all sources with a sector matching /Coal/ regex
 coal <- scs[sapply(scs$EI.Sector, function(s) { grepl("Coal", s) }),]
-codes <- unique(coal$SCC)
+codes <- as.character(unique(coal$SCC))
 
-# sum emissions by year
+# create lookup data frame, mapping SCC code to sector
+lookup <- unique(subset(coal[which(coal$SCC %in% codes),],
+                        select=c(SCC, EI.Sector)))
+
+# get data with SCC that maps to a Coal code
+data <- df[df$SCC %in% codes,]
+
+# add sector from lookup
+data$EI.Sector <- sapply(data$SCC,
+                         function(scc) {
+                             lookup[lookup$SCC == scc,]$EI.Sector })
+
+# sum emissions by year and sector
 agg <- aggregate(data$Emissions,
-                 by=list(year = data$year, type = data$type),
+                 by=list(year = data$year, EI.Sector = data$EI.Sector),
                  FUN=sum,
                  na.rm=TRUE)
 
 # plot
-png(filename="plot4.png", height=480, width=480)
-qplot(year,
-      x,
-      data=agg,
-      geom="line",
-      xlab="Year",
-      ylab="Total PM2.5 Emissions",
-      color=type)
+png(filename="plot5.png", height=480, width=480)
+p <- qplot(year,
+           log(x, base=10),
+           data=agg,
+           geom="line",
+           color=EI.Sector,
+           xlab="Year",
+           ylab="Total PM2.5 Emissions from Coal (log)")
+p+theme(legend.direction="vertical",
+        legend.position="bottom")
 dev.off()
